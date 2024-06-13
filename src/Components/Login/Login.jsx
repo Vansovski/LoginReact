055b9719a40ce6c73d88060c588791cf401b0a03
Marcelo Vansovski de Melo <53240540+Vansovski@users.noreply.github.com>
2024-06-12 21:20:07 -0300

    Captura de dados

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,37 @@
 import {FaUser, FaLock} from "react-icons/fa"
 
+import { useState } from "react"
+
+import "./Login.css"
+
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(username, password);
+        console.log("Envio");
+    }
+
   return (
     <div className="container">
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <h1>Acesse o sistema</h1>
             <div>
-                <input type="email" placeholder='E-mail' />
+                <input 
+                type="email" 
+                placeholder='E-mail'
+                onChange={(e) => setUsername(e.target.value)} />
                 <FaUser className="icon"/>
             </div>
             <div>
-                <input type="password" name="password" placeholder='Senha'/>
+                <input 
+                type="password" 
+                name="password" 
+                placeholder='Senha'
+                onChange={(e) => setPassword(e.target.value)}/>
                 <FaLock className="icon"/>
             </div>
             <div className="recall-forget">
